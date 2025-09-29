54894a52b1b33135475fc8d820da7dea87c0d7f9
victorpaularony <victorpaularony@gmail.com>
2025-09-29 15:10:03 +0300

    feat(rabbitmq-config): add rabbitmq-configuration module
    This commit introduces the `rabbitmq-configuration` module, including the main application class, pom.xml, and other necessary files.

---
+++ rabbitMQ/sample2/rabbitMQ-configuration/src/main/java/com/rabbitMQ_configuration/RabbitMqConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitMQ_configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqConfigurationApplication.class, args);
+	}
+
+}
---
+++ rabbitMQ/sample2/rabbitMQ-configuration/src/test/java/com/rabbitMQ_configuration/RabbitMqConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitMQ_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

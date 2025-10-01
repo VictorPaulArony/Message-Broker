a2dcd073e1fc0a0fb23a73c017f7aa36763c7ffd
victorpaularony <victorpaularony@gmail.com>
2025-10-01 18:19:42 +0300

    feat: implement user registration logic

--- rabbitMQ/sample2/registration-service/src/main/java/com/registration_service/service/RegistrationService.java
+++ rabbitMQ/sample2/registration-service/src/main/java/com/registration_service/service/RegistrationService.java
@@ -1,6 +1,14 @@
 package com.registration_service.service;
 
+import javax.management.RuntimeErrorException;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.registration_service.config.PasswordEncoderConfig;
+import com.registration_service.dto.UserRegistrationEvent;
 import com.registration_service.dto.UserRequest;
+import com.registration_service.exception.FieldAlreadyExistsException;
+import com.registration_service.model.User;
 import com.registration_service.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -10,10 +18,36 @@ import lombok.RequiredArgsConstructor;
 public class RegistrationService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public User registerUser(UserRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new FieldAlreadyExistsException("email",request.getEmail());
+        }
+
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new FieldAlreadyExistsException("username", request.getUsername());
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(encodedPassword);
         
+        User savedUser = userRepository.save(user);
+
+        //send registion event to rabbitMQ
+        UserRegistrationEvent event = new UserRegistrationEvent();
+        event.getUserId();
+        event.getUsername();
+        event.getEmail();
+
+        return savedUser;
+    }
+
+    
 
         
 

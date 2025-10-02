483fb2f59d149bdcd5fa60295461d1ef1dbb5b89
victorpaularony <victorpaularony@gmail.com>
2025-10-02 12:00:35 +0300

    feat(registration): implement user registration service

--- rabbitMQ/sample2/registration-service/src/main/java/com/registration_service/service/RegistrationService.java
+++ rabbitMQ/sample2/registration-service/src/main/java/com/registration_service/service/RegistrationService.java
@@ -1,29 +1,32 @@
 package com.registration_service.service;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-import com.registration_service.config.PasswordEncoderConfig;
 import com.registration_service.dto.UserRegistrationEvent;
 import com.registration_service.dto.UserRequest;
 import com.registration_service.exception.FieldAlreadyExistsException;
 import com.registration_service.model.User;
+import com.registration_service.producer.RegistrationMessageProducer;
 import com.registration_service.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Service
 public class RegistrationService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RegistrationMessageProducer registrationMessageProducer;
 
     @Transactional
     public User registerUser(UserRequest request) {
+        System.out.println("Registering user: " + request.getUsername());
+        
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new FieldAlreadyExistsException("email",request.getEmail());
+            throw new FieldAlreadyExistsException("email", request.getEmail());
         }
 
         if (userRepository.existsByUsername(request.getUsername())) {
@@ -35,21 +38,19 @@ public class RegistrationService {
         user.setUsername(request.getUsername());
         user.setEmail(request.getEmail());
         user.setPassword(encodedPassword);
-        
+
         User savedUser = userRepository.save(user);
 
-        //send registion event to rabbitMQ
+        // send registion event to rabbitMQ
         UserRegistrationEvent event = new UserRegistrationEvent();
-        event.getUserId();
-        event.getUsername();
-        event.getEmail();
+        event.setUserId(savedUser.getId());
+        event.setUsername(savedUser.getUsername());
+        event.setEmail(savedUser.getEmail());
+
+        registrationMessageProducer.sendRegistrationEvent(event);
+        System.out.println("ok");
 
         return savedUser;
     }
 
-    
-
-        
-
-    
 }

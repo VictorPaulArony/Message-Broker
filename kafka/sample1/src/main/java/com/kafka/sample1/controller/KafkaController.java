bc94ab6d293113a75b6f528e3d2ea26bd0f367f3
victorpaularony <smallypauls@gmail.com>
2025-10-03 13:22:09 +0300

    refactor: update KafkaController

--- kafka/sample1/src/main/java/com/kafka/sample1/controller/KafkaController.java
+++ kafka/sample1/src/main/java/com/kafka/sample1/controller/KafkaController.java
@@ -1,4 +1,22 @@
 package com.kafka.sample1.controller;
 
+import com.kafka.sample1.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/kafka/")
 public class KafkaController {
+
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
+
+    @PostMapping("/publish")
+    public String handleMessage(@RequestParam String message) {
+        kafkaProducerService.sendMessage(message);
+        return "Message published: " + message;
+    }
 }

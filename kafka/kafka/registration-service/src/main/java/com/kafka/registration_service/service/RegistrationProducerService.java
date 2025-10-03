d25ea6f5d091659892fa96b97e00d5d8326b61c7
victorpaularony <smallypauls@gmail.com>
2025-10-04 02:59:18 +0300

    refactor(registration-service): Update kafka topic name from value annotation

--- kafka/kafka/registration-service/src/main/java/com/kafka/registration_service/service/RegistrationProducerService.java
+++ kafka/kafka/registration-service/src/main/java/com/kafka/registration_service/service/RegistrationProducerService.java
@@ -6,8 +6,10 @@ import com.kafka.registration_service.dto.UserRegistrationRequest;
 import com.kafka.registration_service.model.UserRegistration;
 import com.kafka.registration_service.repository.UserRepository;
 
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,8 @@ public class RegistrationProducerService {
     private final UserRepository userRepository;
     private final KafkaTemplate<String, UserRegistrationEvent> kafkaTemplate;
     private final PasswordEncoderConfig passwordEncoderConfig;
+    @Value("${kafka.topic.name}")
+    private String topicName;
 
     public UserRegistration registerUser(UserRegistrationRequest request) {
         if (userRepository.existsByEmail(request.getEmail()) || userRepository.existsByUserName(request.getUserName())) {
@@ -34,23 +38,25 @@ public class RegistrationProducerService {
         user.setPassword(encryptPass);
         user.setTimeStamp(new Date());
 
+
+
         UserRegistrationEvent event = new UserRegistrationEvent();
                 event.setUserId(user.getUserId());
                 event.setUserName(user.getUserName());
                 event.setEmail(user.getEmail());
 
-        kafkaTemplate.send("${kafka.topic.name}", event )
+        kafkaTemplate.send(topicName, event )
                 .whenComplete((result, ex) -> {
                     if (ex == null) {
                         log.info("Sent message=[{}] with offset=[{}]",
-                                event.getUserId(),
+                                event.getUserName(),
                                 result.getRecordMetadata().offset());
                     } else {
                         log.error("Unable to send message=[{}] due to: {}",
-                                event.getUserId(),
+                                event.getUserName(),
                                 ex.getMessage());
                     }
-
+                    log.info("registration message sent to the kafka ok {}", event);
                 });
         return user;
 

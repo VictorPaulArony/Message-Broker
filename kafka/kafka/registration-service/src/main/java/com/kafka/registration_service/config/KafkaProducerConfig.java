2c9829dc255bee82f1857e8ddcc1af37f3167738
victorpaularony <smallypauls@gmail.com>
2025-10-04 01:00:13 +0300

    feat(registration-service): Add kafka topic configuration

--- kafka/kafka/registration-service/src/main/java/com/kafka/registration_service/config/KafkaProducerConfig.java
+++ kafka/kafka/registration-service/src/main/java/com/kafka/registration_service/config/KafkaProducerConfig.java
@@ -1,4 +1,22 @@
 package com.kafka.registration_service.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaProducerConfig {
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
+
+    @Bean
+    public NewTopic userRegistrationTopic() {
+        return TopicBuilder.name(topicName)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
 }

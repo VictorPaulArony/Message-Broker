9b6ed23aa8c7d8b102a132b04c4d2e0f72e42dae
victorpaularony <smallypauls@gmail.com>
2025-10-04 01:01:53 +0300

    feat(registration-service): Add user registration entity

--- kafka/kafka/registration-service/src/main/java/com/kafka/registration_service/model/UserRegistration.java
+++ kafka/kafka/registration-service/src/main/java/com/kafka/registration_service/model/UserRegistration.java
@@ -1,4 +1,24 @@
 package com.kafka.registration_service.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+@AllArgsConstructor
 public class UserRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+    private String email;
+    private String password;
+    private Date timeStamp;
 }
